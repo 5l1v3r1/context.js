1529f5a62630f53a3cde7ac5ca95f6332a80405a
Alex Nichol <unixpickle@gmail.com>
2015-05-16 13:22:34 -0400

    drafted ExpandableRow

--- src/row.js
+++ src/row.js
@@ -16,7 +16,7 @@ function TextRow(text, style) {
 }
 
 TextRow.prototype.element = function() {
-  return this._$element;
+  return this._$element[0];
 };
 
 TextRow.prototype.minimumWidth = function() {
@@ -42,6 +42,49 @@ TextRow.prototype._measureWidth = function() {
     visibility: 'visible'
   });
   return result;
+};
+
+function ExpandableRow(text, style) {
+  TextRow.call(this, text, style);
+  this._$arrow = $('<canvas></canvas>').css({
+    width: ExpandableRow.ARROW_WIDTH,
+    height: ExpandableRow.ARROW_HEIGHT,
+    position: 'absolute',
+    right: DEFAULT_STYLE.paddingRight,
+    top: 'calc(50% - ' + ExpandableRow.ARROW_HEIGHT/2 + 'px)'
+  });
+  this._$element.css({minWidth: this.minimumWidth()});
+  this._$element.append(this._$arrow);
+  this._fillCanvas();
+}
+
+ExpandableRow.ARROW_WIDTH = 20;
+ExpandableRow.ARROW_HEIGHT = 20;
+ExpandableRow.THICKNESS = 2;
+
+ExpandableRow.prototype = Object.create(TextRow.prototype);
+
+ExpandableRow.prototype.minimumWidth = function() {
+  return ExpandableRow.prototype.minimumWidth.call(this) + ;
+};
+
+ExpandableRow.prototype._fillCanvas = function() {
+  var context = this._$arrow[0].getContext('2d');
+  var ratio = Math.ceil(window.crystal.getRatio());
+  var width = ratio * ExpandableRow.ARROW_WIDTH;
+  var height = ratio * ExpandableRow.ARROW_HEIGHT;
+  this._$arrow[0].width = width;
+  this._$arrow[0].height = height;
+  
+  context.fillStyle = '#999999';
+  context.lineWidth = ratio*ExpandableRow.THICKNESS;
+  context.beginPath();
+  context.moveTo(ratio*ExpandableRow.THICKNESS, ratio*ExpandableRow.THICKNESS);
+  context.lineTo(width-ratio*ExpandableRow.THICKNESS, height/2);
+  context.lineTo(ratio*ExpandableRow.THICKNESS,
+    height-ratio*ExpandableRow.THICKNESS);
+  context.stroke();
+  context.closePath();
 };
 
 exports.TextRow = TextRow;
